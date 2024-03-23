bc7e55a54b75fadb03d2a81f5cc7880bdb39fccb
Ingrid Brito <brito.ingrid2202@gmail.com>
2024-03-23 18:13:35 -0300

    Criação de Seeders e da tabela de episódios

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,9 +1,10 @@
 // array de todas as opções de models que vamos definir
 
 import { ResourceWithOptions } from "adminjs";
-import { Category, Course } from "../../models";
+import { Category, Course, Episode } from "../../models";
 import { categoryResourceOptions } from "./category";
 import { courseResourceOptions } from "./course";
+import { episodeResourceOptions } from "./episode";
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
@@ -14,4 +15,8 @@ export const adminJsResources: ResourceWithOptions[] = [
     resource: Category,
     options: categoryResourceOptions
   },
+  {
+    resource: Episode,
+    options: episodeResourceOptions
+  }
 ]
