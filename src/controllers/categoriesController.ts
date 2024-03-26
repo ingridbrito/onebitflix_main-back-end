492f5214b7af234c34302f2b1aee3ebb27efbe51
Ingrid Brito <brito.ingrid2202@gmail.com>
2024-03-26 18:52:39 -0300

    Obtendo cursos de uma categoria

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -2,13 +2,14 @@ import { Request, Response } from "express";
 import { Category } from "../models";
 import { categoryService } from "../services/categoryService";
 import { getPaginationParams } from "../helpers/getPaginationParams";
+import { Error } from "sequelize";
 
 export const categoriesController = {
     index: async (req: Request, res: Response) => {
         const [page, perPage] = getPaginationParams(req.query)
         try {
            const paginatedCategories  = await categoryService.findAllPaginated(page, perPage)
-//quando se trabalha com paginação deve se informar em que lugar a pessoa está
+          //quando se trabalha com paginação deve se informar em que lugar a pessoa está
             return res.json(paginatedCategories)
 
         } catch (err) {
@@ -17,6 +18,19 @@ export const categoriesController = {
             }
         }
 
+    },
+
+    show: async(req: Request, res: Response) => {
+        const { id } = req.params
+
+        try {
+            const category = await categoryService.findByIdWithCourses(id)
+            return res.json(category)
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
     }
 }
 
--- src/services/categoryService.ts
+++ src/services/categoryService.ts
@@ -18,5 +18,22 @@ export const categoryService = {
             perPage,
             total: count
           }
+    },
+
+    findByIdWithCourses: async (id: string) => {
+      const categoryWithCourses = await Category.findByPk(id, {
+        attributes: ['id', 'name'], 
+        include:{
+          association: 'courses',
+          attributes: [
+            'id',
+            'name',
+            'synopsis',
+            ['thumbnail_url', 'thumbnailUrl']
+          ]
+        }
+      })
+      return categoryWithCourses
+      //procura o curso dentro da categoria, segundo o id e com os parâmetros
     }
 }
