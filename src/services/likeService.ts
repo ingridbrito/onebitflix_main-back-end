34bcffb66e7d16b0625279f45e0af76286760fc6
Ingrid Brito <brito.ingrid2202@gmail.com>
2024-03-30 15:49:11 -0300

    Atualizando o endpoint de cursos

--- src/services/likeService.ts
+++ src/services/likeService.ts
@@ -17,6 +17,18 @@ export const likeService = {
                 courseId
               }
         })
+    },
+
+    //GET /courses/:id
+
+    isLiked: async (userId: number, courseId: number) => {
+        const like = await Like.findOne({
+            where: {
+                userId,
+                courseId
+            }
+        })
+        return like !== null ? true : false
     }
 
 }
