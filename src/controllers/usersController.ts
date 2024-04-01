3265453b18943bbe7093fd53c01e11cf461cb19f
Ingrid Brito <brito.ingrid2202@gmail.com>
2024-04-01 18:49:51 -0300

    Atualizando informações do usuário

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -5,7 +5,7 @@ import { userService } from "../services/userService"
 export const userController = {
     //GET /users/current/watching
     watching: async (req: AuthenticatedRequest, res: Response) => {
-        const { id } =req.user!
+        const { id } = req.user!
 
         try {
             const watching = await userService.getKeepWatchingList(id)
@@ -18,15 +18,37 @@ export const userController = {
     },
 
     //GET /users/current
-    show: async (req:AuthenticatedRequest, res: Response) =>{
-      const currentUser = req.user!
+    show: async (req: AuthenticatedRequest, res: Response) => {
+        const currentUser = req.user!
 
-      try {
-        return res.json(currentUser)
-      } catch (err) {
-        if (err instanceof Error) {
-            return res.status(400).json({ message: err.message })
+        try {
+            return res.json(currentUser)
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
         }
-      }
+    },
+
+    //PUT /users/current
+
+    update: async (req: AuthenticatedRequest, res: Response) => {
+        const { id } = req.user!
+        const { firstName, lastName, phone, email, birth } = req.body
+        try {
+            const updatedUser = await userService.update(id, {
+                firstName,
+                lastName,
+                phone,
+                email,
+                birth
+            })
+
+            return res.json(updatedUser)
+        }catch (err){
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
+        }
     }
 }
